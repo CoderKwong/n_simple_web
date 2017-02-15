5893f1ae231f8eecd40c7ec956d6089dddc10b70
kwonghinho <jiangyanhao1991@gmail.com>
2017-02-15 21:57:39 +0800

    终于可以了，都是服务器没有打开端口的缘故

--- server.js
+++ server.js
@@ -20,9 +20,6 @@ http.createServer(function (req, res) {
         res.statusCode = 404
         res.end('no such location')
     });
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
 }).listen(7777)
 
 handler.on('error', function (err) {
