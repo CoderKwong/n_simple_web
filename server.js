7c1b1c8edc4a960c40d04a149aed03f6bb9c9303
kwonghinho <jiangyanhao1991@gmail.com>
2017-02-15 21:59:37 +0800

    测试

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/', secret: 'kwong' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
-//
-// function run_cmd(cmd, args, callback) {
-//     var spawn = require('child_process').spawn;
-//     var child = spawn(cmd, args);
-//     var resp = "";
-//
-//     child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
-//     child.stdout.on('end', function() { callback (resp) });
-// }
+
+function run_cmd(cmd, args, callback) {
+    var spawn = require('child_process').spawn;
+    var child = spawn(cmd, args);
+    var resp = "";
+
+    child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
+    child.stdout.on('end', function() { callback (resp) });
+}
 
 http.createServer(function (req, res) {
     handler(req, res, function (err) {
@@ -31,5 +31,5 @@ handler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
         event.payload.ref);
-    // run_cmd('sh', ['./deploy.sh',event.payload.repository.name], function(text){ console.log(text) });
+    run_cmd('sh', ['./deploy.sh',event.payload.repository.name], function(text){ console.log(text) });
 })
